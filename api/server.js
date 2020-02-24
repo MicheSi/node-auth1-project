315ff4cae8c3162a9714d8616ec4b71d978ea4ad
mlazzell17@gmail.com <MicheSi>
2020-02-24 13:54:38 -0700

    installed helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const apiRouter = require('./api-router');
+const configMiddleware = require('./config-middleware');
 
 const server = express();
+
+configMiddleware(server);
 
 server.use('/api', apiRouter);
 
